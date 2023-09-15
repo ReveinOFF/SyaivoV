32dbbe0463aa97e3a314dfcdc13739424c5b362c
RonniePlay <ronnieplayyt@gmail.com>
2023-09-15 14:49:35 +0300

    Update Contact Page

--- Front/src/pages/contact/contact.jsx
+++ Front/src/pages/contact/contact.jsx
@@ -16,41 +16,55 @@ const Contact = () => {
       <div className="contact">
         <div className="info">
           <div>
-            <img src={officeIcon} alt="office" />
+            <h2>Наш офіс тут:</h2>
             <div>
-              м. Рівне, Рівненська обл.,
-              <br />
-              вул. Відінська, 39б.
+              <img src={officeIcon} alt="office" />
+              <div>
+                м. Рівне, Рівненська обл.,
+                <br />
+                вул. Відінська, 39б.
+              </div>
             </div>
           </div>
+
           <div>
-            <img src={timeIcon} alt="time" />
-            <div>ПН-ПТ: 08:30-17:30</div>
-          </div>
-          <div>
-            <img src={homePhoneIcon} alt="home phone" />
-            <div>(0362) 62-98-08</div>
-          </div>
-          <div>
-            <img src={mobilePhoneIcon} alt="mobile phone" />
+            <h2>Контактна інформація:</h2>
             <div>
-              <Link to="tel:+380677336594">(067) 733-65-94</Link>
-              <Link to="tel:+380963282889">(096) 328-28-89</Link>
-              <Link to="tel:+380503219592">(050) 321-95-92</Link>
+              <img src={homePhoneIcon} alt="home phone" />
+              <div>(0362) 62-98-08</div>
+            </div>
+            <div>
+              <img src={mobilePhoneIcon} alt="mobile phone" />
+              <div>
+                <Link to="tel:+380677336594">(067) 733-65-94</Link>
+                <Link to="tel:+380963282889">(096) 328-28-89</Link>
+                <Link to="tel:+380503219592">(050) 321-95-92</Link>
+              </div>
+            </div>
+            <div>
+              <img src={mailIcon} alt="mail" />
+              <Link to="mailto:ppsyaivo-v@ukr.net">ppsyaivo-v@ukr.net</Link>
             </div>
           </div>
+
           <div>
-            <img src={mailIcon} alt="mail" />
-            <Link to="mailto:ppsyaivo-v@ukr.net">ppsyaivo-v@ukr.net</Link>
+            <h2>РЕЖИМ РОБОТИ:</h2>
+            <div>
+              <img src={timeIcon} alt="time" />
+              <div>
+                <div>ПН-ПТ: 08:30-17:30</div>
+                <div>СБ-НД: Вихідний</div>
+              </div>
+            </div>
           </div>
         </div>
 
         <form className="snd-msg" onSubmit={(e) => handleSubmite(e)}>
+          <div>Ви також можете відправити лист з вашим зверненням.</div>
           <div>
-            Ви також можете відправити лист на нашу електронну скриньку.
+            <input type="text" name="name" placeholder="Ваше ім'я" />
+            <input type="email" name="email" placeholder="E-mail адреса" />
           </div>
-          <input type="text" name="name" placeholder="Ваше ім'я" />
-          <input type="email" name="email" placeholder="E-mail адреса" />
           <input type="text" name="subject" placeholder="Тема повідомлення" />
           <textarea
             name="message"
@@ -62,6 +76,8 @@ const Contact = () => {
         </form>
       </div>
 
+      <hr className="hr-block" />
+
       <iframe
         className="map"
         src="https://maps.google.com/maps?q=Сяйво-В&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
@@ -69,7 +85,7 @@ const Contact = () => {
         width="100%"
         height={250}
         allowFullScreen
-      ></iframe>
+      />
     </>
   );
 };
