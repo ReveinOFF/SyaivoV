54a65ba1a854978b12d0a76da286a21c64fe3b54
RonniePlay <ronnieplayyt@gmail.com>
2023-10-10 02:26:17 +0300

    Add mysql

--- Back/service/db.js
+++ Back/service/db.js
@@ -1,5 +1,13 @@
+// const mysql = require("mysql2");
 const Pool = require("pg").Pool;
 require("dotenv").config();
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "usersdb2",
+//   password: "123456",
+// });
 
 const pool = new Pool({
   user: process.env.DB_USER,
