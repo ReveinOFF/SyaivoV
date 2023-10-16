885f6dd5ee74c4f68694318bf301775e9f1c2a6c
RonniePlay <ronnieplayyt@gmail.com>
2023-10-16 21:28:25 +0300

    Change auth system and add change pass

--- Back/routes/auth.js
+++ Back/routes/auth.js
@@ -1,11 +1,22 @@
 const express = require("express"),
   router = express.Router(),
-  jwt = require("jsonwebtoken");
+  jwt = require("jsonwebtoken"),
+  fs = require("fs"),
+  bcrypt = require("bcrypt");
 
 require("dotenv").config();
 
 const tokenKeyAuth = process.env.JWT_SECRET,
   emailStatic = process.env.EMAIL_TO;
+
+function readUsers() {
+  const usersData = fs.readFileSync("admin.json");
+  return JSON.parse(usersData);
+}
+
+function writeUsers(users) {
+  fs.writeFileSync("admin.json", JSON.stringify(users));
+}
 
 router.post("/", (req, res) => {
   if (!req.body || !req.body.login || !req.body.password)
@@ -14,7 +25,12 @@ router.post("/", (req, res) => {
   const { login, password } = req.body;
 
   try {
-    if (login == process.env.ADMIN_LOGIN && password == process.env.ADMIN_PASS)
+    const users = readUsers();
+
+    if (
+      login == process.env.ADMIN_LOGIN &&
+      bcrypt.compareSync(password, users.password)
+    )
       return res
         .status(200)
         .json(jwt.sign({ email: emailStatic, login: login }, tokenKeyAuth));
@@ -22,6 +38,27 @@ router.post("/", (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json("Помилка аторизації!");
+  }
+});
+
+router.post("/change-password", (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  try {
+    const users = readUsers();
+
+    if (!bcrypt.compareSync(oldPassword, users.password)) {
+      res.status(404).json("Пароль вказано не вірно!");
+      return;
+    }
+
+    const hashedPassword = bcrypt.hashSync(newPassword, 10);
+    users.password = hashedPassword;
+    writeUsers(users);
+
+    res.status(201).json("Пароль змінено!");
+  } catch (error) {
+    res.status(500).json("Помилка в змінені пароля!");
   }
 });
 
