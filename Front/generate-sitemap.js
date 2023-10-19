7a0f32508ef8c0c5abee5f4843e9e56e94b9dcb5
RonniePlay <ronnieplayyt@gmail.com>
2023-10-19 22:41:31 +0300

    Update sitemap generator

--- Front/generate-sitemap.js
+++ Front/generate-sitemap.js
@@ -3,25 +3,34 @@ const fs = require("fs");
 const baseUrl = "http://syaivo.rivne.ua";
 
 const paths = [
-  "/",
-  "/catalog",
-  "/products",
-  "/contact",
-  "/company",
-  "/size",
-  "/size/clothing",
-  "/size/boots",
+  { path: "/", changefreq: "weekly", priority: 1 },
+  { path: "/catalog", changefreq: "never", priority: 0.8 },
+  { path: "/catalog/protected", changefreq: "weekly", priority: 0.8 },
+  { path: "/catalog/boots", changefreq: "weekly", priority: 0.8 },
+  { path: "/catalog/clothing", changefreq: "weekly", priority: 0.8 },
+  { path: "/products", changefreq: "weekly", priority: 0.8 },
+  { path: "/products/gloves", changefreq: "weekly", priority: 0.8 },
+  { path: "/products/household", changefreq: "weekly", priority: 0.8 },
+  { path: "/products/embroidery", changefreq: "weekly", priority: 0.8 },
+  { path: "/products/clothing", changefreq: "weekly", priority: 0.8 },
+  { path: "/products/boots", changefreq: "weekly", priority: 0.8 },
+  { path: "/products/protected", changefreq: "weekly", priority: 0.8 },
+  { path: "/contact", changefreq: "never", priority: 0.8 },
+  { path: "/company", changefreq: "never", priority: 0.8 },
+  { path: "/size", changefreq: "never", priority: 0.8 },
+  { path: "/size/clothing", changefreq: "never", priority: 0.8 },
+  { path: "/size/boots", changefreq: "never", priority: 0.8 },
 ];
 
 const xmlData = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${paths
       .map(
-        (path) => `
+        (item) => `
       <url>
-        <loc>${baseUrl}${path}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
+        <loc>${baseUrl}${item.path}</loc>
+        <changefreq>${item.changefreq}</changefreq>
+        <priority>${item.priority}</priority>
       </url>
     `
       )
