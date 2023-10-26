16b2b5957e401597de4b2683c04a3a87ad013cf5
RonniePlay <ronnieplayyt@gmail.com>
2023-10-26 19:08:34 +0300

    Modified under ssl

--- Front/generate-sitemap.js
+++ Front/generate-sitemap.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const baseUrl = "http://syaivo.rivne.ua";
+const baseUrl = "https://syaivo.rivne.ua";
 
 const paths = [
   { path: "/", changefreq: "weekly", priority: 1 },
